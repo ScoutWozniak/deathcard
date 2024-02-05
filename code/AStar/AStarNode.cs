84bd8665fd7c3e3296b64822d3d43827d672e25c
yuberee <59583743+yuberee@users.noreply.github.com>
2024-02-05 22:07:10 +0100

    AStarNode hashcode fix

--- code/AStar/AStarNode.cs
+++ code/AStar/AStarNode.cs
@@ -3,6 +3,7 @@
 public struct AStarNode : IHeapItem<AStarNode>, IEquatable<AStarNode>
 {
 	public IVoxel Current { get; internal set; } = null;
+	public Vector3S Position { get; internal set; }
 	public float gCost { get; internal set; } = 0f;
 	public float hCost { get; internal set; } = 0f;
 	public float fCost => gCost + hCost;
@@ -18,7 +19,15 @@ public struct AStarNode : IHeapItem<AStarNode>, IEquatable<AStarNode>
 		return -compare;
 	}
 
-	public override int GetHashCode() => Current.GetHashCode();
+	public override int GetHashCode()
+	{
+		var currentHash = Current.GetHashCode();
+		var positionHash = Position.GetHashCode();
+		var gCostHash = gCost.GetHashCode();
+		var hCostHash = hCost.GetHashCode();
+
+		return HashCode.Combine( currentHash, positionHash, gCostHash, hCostHash );
+	}
 
 	public static bool operator ==( AStarNode a, AStarNode b ) => a.Equals( b );
 	public static bool operator !=( AStarNode a, AStarNode b ) => !a.Equals( b );
@@ -26,22 +35,11 @@ public struct AStarNode : IHeapItem<AStarNode>, IEquatable<AStarNode>
 	public override bool Equals( object obj )
 	{
 		if ( obj is not AStarNode node ) return false;
-
-		if ( node.Current != Current ) return false;
-		if ( node.gCost != gCost ) return false;
-		if ( node.hCost != hCost ) return false;
-		if ( node.HeapIndex != HeapIndex ) return false;
+		if ( node.GetHashCode() != GetHashCode() ) return false;
 
 		return true;
 	}
 
 	public bool Equals( AStarNode other )
-	{
-		if ( other.Current != Current ) return false;
-		if ( other.gCost != gCost ) return false;
-		if ( other.hCost != hCost ) return false;
-		if ( other.HeapIndex != HeapIndex ) return false;
-
-		return true;
-	}
+		=> GetHashCode() == other.GetHashCode();
 }
